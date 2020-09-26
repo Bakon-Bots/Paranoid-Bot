cfdfca0fbc39d55ee86c8b6c2f327a3d7497b9ec
Christian <cjbrown0718@gmail.com>
2020-09-26 18:32:57 -0500

    some logging not all

--- bot.py
+++ bot.py
@@ -11,14 +11,6 @@ client = commands.Bot(command_prefix=os.getenv('PREFIX'))
 async def on_ready():
   print('hello!')
 
-@client.event
-async def on_message_edit(before, after):
-  message_embed = discord.Embed(title=f'Message edited in {before.channel.mention} sent by {before.author.mention}')
-  messagelog = client.get_channel(757793206384721950)
-  print(before)
-  print(after)
-  await messagelog.send(f'```Message sent by {before.author.mention} edited in {before.channel.mention} \nBefore: \'{before.content}\' \nAfter: \'{after.content}\'```')
-
 for filename in os.listdir('./cogs'):
   if filename.endswith('.py'):
     client.load_extension(f'cogs.{filename[:-3]}')
---
+++ cogs/logging.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+
+
+
+class Logging(commands.Cog):
+  def __init__(self, client):
+    self.client = client
+
+  @commands.Cog.listener()
+  async def on_message_delete(self, message):
+    messagelog = self.client.get_channel(757793206384721950)
+    deleted = discord.Embed(
+        description=f"Message deleted in {message.channel.mention}", color=0x4287f5
+    ).set_author(name=message.author, url=discord.Embed.Empty, icon_url=message.author.avatar_url)
+
+    deleted.add_field(name="Message", value=message.content)
+    deleted.timestamp = message.created_at
+    await messagelog.send(embed=deleted)
+
+  @commands.Cog.listener()
+  async def on_message_edit(self, before, after):
+    messagelog = self.client.get_channel(757793206384721950)
+    print(before)
+    print(after)
+    edit = discord.Embed(
+        description=f"Message edited in {before.channel.mention}", color=0x00b7ff
+    ).set_author(name=before.author, url=discord.Embed.Empty, icon_url=before.author.avatar_url)
+
+    edit.add_field(name="Old", value=before.content)
+    edit.add_field(name="New", value=after.content)
+    edit.timestamp = before.created_at
+    await messagelog.send(embed=edit)
+
+def setup(client):
+  client.add_cog(Logging(client))
